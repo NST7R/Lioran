a51afe20755e817467547624c859f4c1e6429afb
Anass <anass.rami6@gmail.com>
2025-06-21 15:40:53 +0200

    Fixing camera and runes issue

---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    public IEnumerator FadeOut()
+    {
+        yield return StartCoroutine(Fade(0f, 1f));
+    }
+
+    public IEnumerator FadeIn()
+    {
+        yield return StartCoroutine(Fade(1f, 0f));
+    }
+
+    private IEnumerator Fade(float from, float to)
+    {
+        float elapsed = 0f;
+        Color c = fadeImage.color;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / fadeDuration);
+            c.a = Mathf.Lerp(from, to, t);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = to;
+        fadeImage.color = c;
+    }
+}
